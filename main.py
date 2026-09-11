d299954bf184882f8b5c84b49f24f1cff8226f92
Mohanad338 <kjhjn192@gmail.com>
2026-09-11 17:36:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def save_last_id(msg_id):
 def translate_to_arabic(text):
     if not text:
         return ""
-    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent"
     headers = {
         "x-goog-api-key": GEMINI_KEY,
         "Content-Type": "application/json"
